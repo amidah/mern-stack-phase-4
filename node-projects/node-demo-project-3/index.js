b837307aa9d5b10486c587ecdb9667d8a21d8971
Ubuntu <labuser@ubuntu2204.3laisap4z41ebja1h34soknywf.rx.internal.cloudapp.net>
2024-05-26 17:40:25 +0000

    removed comments

--- node-projects/node-demo-project-3/index.js
+++ node-projects/node-demo-project-3/index.js
@@ -31,67 +31,67 @@ server.on("request", (req, res) => {
 
     const uri = req.url;
 
-    // // Accessing request headers of incoming requests
-    // const { headers } = req;
+    // Accessing request headers of incoming requests
+    const { headers } = req;
 
-    // // Getting user-agent value from request header
-    // const userAgent = headers['user-agent'];
+    // Getting user-agent value from request header
+    const userAgent = headers['user-agent'];
 
-    // // Setting the response headers
-    // if(url === '/textcontent'){
-    //     res.setHeader('Content-Type', 'text/plain');
-    //     res.setHeader('Date', new Date());
-    //     res.end("Welcome to my App!");
-    // }
+    // Setting the response headers
+    if(uri === '/textcontent'){
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Date', new Date());
+        res.end("Welcome to my App!");
+    }
 
-    // if(url === '/jsoncontent'){
+    if(uri === '/jsoncontent'){
    
-    // res.setHeader("Content-Type", "application/json");
-    // res.setHeader('Date', new Date());
-    // res.end(JSON.stringify({
-    //     "platform": process.platform,
-    //     "userAgent": userAgent,
-    //     "date": new Date(),
-    //     "message": "Hellos"
-    // })); 
-    // }
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader('Date', new Date());
+    res.end(JSON.stringify({
+        "platform": process.platform,
+        "userAgent": userAgent,
+        "date": new Date(),
+        "message": "Hellos"
+    })); 
+    }
 
-    // if(url === '/htmlcontent'){
-    //     res.setHeader('Content-Type', 'text/html');
-    //     res.setHeader('Date', new Date());
-    //     res.write('<html>');
-    //     res.write('<body>');
-    //     res.write('<h3>This is an HTML Response !</h3>');
-    //     res.write('</body>');
-    //     res.write('</html>');
-    //     res.end();
-    // }
+    if(uri === '/htmlcontent'){
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Date', new Date());
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<h3>This is an HTML Response !</h3>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
 
-    // if(url === '/api'){
-    //     // redirect
-    //     res.writeHead(302, {
-    //         location: '/jsoncontent'
-    //     })
-    //     return res.end();
+    if(uri === '/api'){
+        // redirect
+        res.writeHead(302, {
+            location: '/jsoncontent'
+        })
+        return res.end();
 
-    // }
+    }
         
-        const pathname = url.parse(uri).pathname;
+    const pathname = url.parse(uri).pathname;
 
-        if(pathname === "/parserequest"){
-        
-            const query = url.parse(uri).query;
-            console.log(`URI => ${uri}`);
-            console.log(`Query => ${query}`);
-            const queryObj = query.split("&").reduce((prev, next) => {
-                let [key, value] = next.split("=");
-                return {...prev, [key]:value}
-            }, {});
+    if(pathname === "/parserequest"){
+    
+        const query = url.parse(uri).query;
+        console.log(`URI => ${uri}`);
+        console.log(`Query => ${query}`);
+        const queryObj = query.split("&").reduce((prev, next) => {
+            let [key, value] = next.split("=");
+            return {...prev, [key]:value}
+        }, {});
 
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            return res.end(JSON.stringify(queryObj));
-        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        return res.end(JSON.stringify(queryObj));
+    }
     
 });
 
